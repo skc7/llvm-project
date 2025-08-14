7e13ca3f0e97fd03153d73c3269b16790b85bb4e
skc7 <Krishna.Sankisa@amd.com>
2025-08-14 09:46:31 +0530

    [Flang] Add flang to mlir lowering for dyn_groupprivate

--- llvm/include/llvm/Frontend/OpenMP/ConstructDecompositionT.h
+++ llvm/include/llvm/Frontend/OpenMP/ConstructDecompositionT.h
@@ -258,6 +258,9 @@ private:
   bool
   applyClause(const tomp::clause::ThreadLimitT<TypeTy, IdTy, ExprTy> &clause,
               const ClauseTy *);
+  bool applyClause(
+      const tomp::clause::DynGroupprivateT<TypeTy, IdTy, ExprTy> &clause,
+      const ClauseTy *);
 
   uint32_t version;
   llvm::omp::Directive construct;
@@ -1143,6 +1146,21 @@ bool ConstructDecompositionT<C, H>::applyClause(
     const ClauseTy *node) {
   // [5.2:340:31]
   if (!applyToAll(node))
+    return error(node, ErrorCode::NoLeafAllowing);
+  return true;
+}
+
+// DYN_GROUPPRIVATE
+// [6.1] dyn_groupprivate clause
+// Directives: target, teams
+//
+// The effect of the dyn_groupprivate clause is as if it is applied to the
+// outermost leaf construct that permits it.
+template <typename C, typename H>
+bool ConstructDecompositionT<C, H>::applyClause(
+    const tomp::clause::DynGroupprivateT<TypeTy, IdTy, ExprTy> &clause,
+    const ClauseTy *node) {
+  if (!applyToOutermost(node))
     return error(node, ErrorCode::NoLeafAllowing);
   return true;
 }
