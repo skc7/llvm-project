e34e542bd57664d8b26ddac6b994eaed5ab7ff26
skc7 <Krishna.Sankisa@amd.com>
2026-01-28 15:04:49 +0530

    clang-format

--- mlir/lib/Dialect/OpenMP/IR/OpenMPDialect.cpp
+++ mlir/lib/Dialect/OpenMP/IR/OpenMPDialect.cpp
@@ -2818,8 +2818,7 @@ void TeamsOp::build(OpBuilder &builder, OperationState &state,
       clauses.reductionVars,
       makeDenseBoolArrayAttr(ctx, clauses.reductionByref),
       makeArrayAttr(ctx, clauses.reductionSyms), clauses.threadLimitVars,
-      clauses.accessGroup, clauses.fallback,
-      clauses.dynGroupprivateSize);
+      clauses.accessGroup, clauses.fallback, clauses.dynGroupprivateSize);
 }
 
 // Verify num_teams clause
